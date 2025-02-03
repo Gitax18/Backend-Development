3c6c894f761ceaa19fe5ef33014d02faa3734976
Gitax18 <lusifer0418@gmail.com>
2025-02-03 21:08:23 +0530

    add routing and parse incoming data

--- 2_Express/1_revision/app.js
+++ 2_Express/1_revision/app.js
@@ -1,16 +1,23 @@
 const express = require("express");
-const http = require("http");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("You are in the middle ware 1");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    `<form action="/product" method="POST"><input type="text" name="name"><input type="submit" value="submit"></form>`
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In another middleware ");
-  res.send({ name: "gitanshu" });
+app.use("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send(`<h1>This is HOME Page</h1>`);
 });
 
 app.listen(3000);
