b9389c3ef906a86af6e9bdb9753dda1f2c1fc5f2
Gitax18 <lusifer0418@gmail.com>
2025-02-07 20:55:58 +0530

    add routes to handle create post

--- 2_Express/2_temp_engine/routers/route.posts.js
+++ 2_Express/2_temp_engine/routers/route.posts.js
@@ -1,11 +1,20 @@
 const express = require("express");
-const { Home, ControlError } = require("../controllers/controller.posts");
+const {
+  Home,
+  ControlError,
+  AddPost,
+  HandlePost,
+} = require("../controllers/controller.posts");
 
 const Router = express.Router();
 
 console.log(Home);
 
 Router.get("/", Home);
+
+Router.get("/create-post", AddPost);
+
+Router.post("/create-post", HandlePost);
 
 Router.get("/*", ControlError);
 
