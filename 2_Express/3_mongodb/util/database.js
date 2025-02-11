fac92ba792485c84230116ba3bebfc8873f4fe6e
Gitax18 <lusifer0418@gmail.com>
2025-02-11 19:47:16 +0530

    add database

--- 2_Express/3_mongodb/util/database.js
+++ 2_Express/3_mongodb/util/database.js
@@ -1,8 +1,30 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+require("dotenv").config();
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+let _db;
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+
+  throw new Error("Database not found !!!!");
+};
+
+function MongoConnect(cb) {
+  MongoClient.connect(process.env.MONGODB_CONNECTION)
+    .then((client) => {
+      console.log("Databse connected successfully\n");
+      _db = client.db("node_shop");
+      cb();
+    })
+    .catch((err) => {
+      console.error(err);
+      throw err;
+    });
+}
+
+exports.MongoConnect = MongoConnect;
+exports.getDb = getDb;
