0fb0ce2d72feff03230f00d22da93dc0993d3ce4
Gitax18 <lusifer0418@gmail.com>
2025-02-14 01:10:08 +0530

    storing user object in request parameter

--- 2_Express/3_mongodb/app.js
+++ 2_Express/3_mongodb/app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.fetchById("67ab5d7e1ffe4acc3e78e418")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
