1b3c5b9c42355ba804481d290fd951bb6ef0393b
Gitax18 <lusifer0418@gmail.com>
2025-02-22 20:47:08 +0530

    add addToCart functionality

--- 2_Express/4_mongoose/models/user.js
+++ 2_Express/4_mongoose/models/user.js
@@ -28,6 +28,38 @@ const UserSchema = new Schema({
   },
 });
 
+UserSchema.methods.addToCart = function (product) {
+  console.log("Product Details: " + product);
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    return cp.productId.toString() === product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity,
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
+
+UserSchema.methods.deleteItemFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
 module.exports = mongoose.model("User", UserSchema);
 
 // const mongodb = require('mongodb');
